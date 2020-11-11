a598e9d07779effd05c2649f591c75dc2a97b761
Tim Urista <timothy.urista@gmail.com>
2020-11-10 17:24:55 -0800

    use simple concurrent checker

--- channels/main.go
+++ channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 // Site status
@@ -13,21 +14,38 @@ const (
 
 func main() {
 	links := []string{
-		"https://google.com",
-		"https://facebook.com",
-		"https://stackoverflow.com",
-		"https://golang.org",
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
 		"http://amazon.com",
+		"http://twitter.com",
 	}
 
 	c := make(chan string)
 
 	for _, link := range links {
 		go checkLink(link, c)
-
-		fmt.Println(<-c)
 	}
 
+	// for range links {
+	// 	fmt.Println(<-c)
+	// }
+
+	// infinite loop
+	// for {
+	// 	go checkLink(<-c, c)
+	// }
+
+	// range over channel
+	// means wait for channel to return
+	// then assign to var l
+	for l := range c {
+		go func(link string) {
+			time.Sleep(time.Second * 5)
+			checkLink(link, c)
+		}(l)
+	}
 }
 
 func checkLink(link string, c chan string) {
@@ -37,8 +55,10 @@ func checkLink(link string, c chan string) {
 	// }
 	// return UP
 	if err != nil {
-		c <- link + " is down"
+		fmt.Println(link + " might be down")
+		c <- link
 	} else {
-		c <- link + " is up"
+		fmt.Println(link + " is up")
+		c <- link
 	}
 }
